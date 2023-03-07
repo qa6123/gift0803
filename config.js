159da0089d0e8706a172c92fd87baaf2c3328678
fennz6123 <anlinh6123@gmail.com>
2023-03-07 23:14:42 +0700

    aa

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bảo Bối 💓💓💓';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng Bảo Bối của anh 🎁';
+const contentCard = 'Nhân ngày 08-03 🎉🎉🎉 anh chúc Bảo Bối bé bỏng của anh một ngày thật xinh đẹp, thật vui vẻ, thật ý nghĩa và thật hạnh phúc bên anh nha 💓 Anh yêu Bảo Bối nhứt lun nha 💓';
+
+// phần dưới dành c ho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'bb1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
